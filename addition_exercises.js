8e37a83d702c647ac415b6cc34f41eda21c6857a
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 12:45:53 +0100

    add addition_exercises

---
+++ addition_exercises.js
@@ -0,0 +1,15 @@
+////ONE WAY TO ADD VARIABLES
+var blueCars = 4;
+var redCars = 5;
+var totalCars = blueCars + redCars;
+console.log("I have " + totalCars + " cars!");
+
+////THE HANDY WAY TO DO VARIABLES
+var tapasCount = 4;
+tapasCount++;
+console.log("I have " + tapasCount + " tapas!");
+
+////A SHORT HAND WAY TO ADD AN ARBITRARY NUMBER TO THE VALUE OF A VARIABLE
+var booksCount = 10;
+booksCount += 20; //adds 20 books
+console.log("I have read " + booksCount + " books!");
