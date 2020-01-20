7a7bebd3ddcd80125377a8ec3f6ca03d4dac06f0
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 13:32:48 +0100

    add operator_exercises

---
+++ operator_exercises.js
@@ -0,0 +1,28 @@
+////EQUALITY OPERATOR
+///Using this operator is simple, just use three equal signs.
+console.log(5 === 5);
+let pears = 40 / 2;
+console.log(20 === pears);
+console.log(10 === 11);
+
+////INEQUALITY OPERATOR
+/To use this operator, you use a single exclamation point followed by two equal signs.
+console.log(9 !== 7);
+let oranges = 5 * 6;
+console.log(30 !== oranges);
+console.log(57 !== 57);
+
+////GREATER THAN AND LESS THAN OPERATORS
+///Use greater than operator via the greater than symbol, and the less than operator via the less than symbol.
+console.log(6 < 4);
+console.log(8 < 13);
+let books = 32;
+console.log(books > 30);
+console.log(books > 50);
+
+////OR EQUAL TO OPERATORS
+///Check for greater than or equal to via greater than symbol followed bt an equal sign, and check for less than or equal to via a less than symbol followed by equal sign.
+console.log(29 <= 94);
+console.log(13 <= 9);
+console.log(32 >= 32);
+console.log(42 >= 85);
