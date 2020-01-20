dbfdf1315f8d94453be45c5ab1ab0f701452ea6d
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 12:53:47 +0100

    add subtraction_exercises

---
+++ subtraction_exercises.js
@@ -0,0 +1,15 @@
+////SUBTRACTING NUMBER - THE LONG WAY
+var bobsApples = 13;
+var stolenApples = 7;
+var remainingApples = bobsApples + stolenApples;
+console.log("After getting robbed, he was left with " + remainingApples + " apples.");
+
+////SHORTHAND WAY OF SUBTRACT 1 FROM THE VALUE OF A VARIABLE
+var waterBottles = 4;
+waterBottles--; //subtracts 1
+console.log("I am down to " + waterBottles + " water bottles!");
+
+////AND SHORTHAND WAY TO SUBTRACT AN ARBITRARY NUMBER FROM THE VALUE OF A VARIABLE
+var apples = 50;
+apples -= 30; //subtracts 30 apples
+console.log("I have " + apples + " apples left!")
