294c268634fedcaf8c37d5b78fef2b33bf18b340
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 13:01:02 +0100

    add multiplication_exercises

---
+++ multiplication_exercises.js
@@ -0,0 +1,10 @@
+////MULTIPLYING NUMBERS
+let tacosPerPerson = 3;
+let people = 20; 
+let tacosNeeded = tacosPerPerson * people;
+console.log("We need " + tacosNeeded + " tacos for the party.");
+
+////THIS IS THE SHORTHAND TO MULTIPLY THE VALUE OF A VARIABLE
+let boxes = 2;
+boxes *= 5; //multiplies the value by 5
+console.log("I have " + boxes + " boxes left!");
