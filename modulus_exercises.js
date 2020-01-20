6db8d0aaa41c5d8d44adf099746bb89e92d8f42c
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 13:09:05 +0100

    add modulus_exercises

---
+++ modulus_exercises.js
@@ -0,0 +1,10 @@
+////MODULUS OPERATOR
+let people = 20;
+let peoplePerCar = 6;
+let peopleInLastCar = people % peoplePerCar;
+console.log("The last car will have " + peopleInLastCar + " people in it.");
+
+////THE SHORT HAND WAY TO PERFORM MODULUS
+let slices = 45;
+slices %= 7; //does modulus 7
+console.log("I have " + slices + " slices leftover!");
