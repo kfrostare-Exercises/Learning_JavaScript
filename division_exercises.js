bcbcd76b7334940cc372f82daa2f5e6f249029d6
Karolina Frostare <karolina.frostare@gmail.com>
2020-01-20 13:04:54 +0100

    add division_exercises

---
+++ division_exercises.js
@@ -0,0 +1,10 @@
+////DIVIDING NUMBERS
+let totalSkittles = 75;
+let kids = 5;
+let skittlesPerKid = totalSkittles / kids;
+console.log("Each kid gets " + skittlesPerKid + " skittles.");
+
+////THE SHORTHAND WAY TO DIVIDE A VARIABLE
+let peanuts = 100;
+peanuts /= 20; //divides the value by 20
+console.log("I have " + peanuts + " peanuts left!");
